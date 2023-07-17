01ce361576ec7b3ee3f409d6664293a0da276689
prince954 <akprince0268@gmail.com>
2023-07-17 17:36:21 +0600

    Project Done

---
+++ assect/js/app.js
@@ -0,0 +1,40 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+
+
+
+// ---------------
+
+$(".count").each(function () {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: 4000,
+        easing: "swing",
+        step: function (now) {
+          now = Number(Math.ceil(now)).toLocaleString('en');
+                                $(this).text(now);
+        },
+      }
+    );
+});
